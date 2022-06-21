2dc427c4b5bd4ebaa28f64ecbd932c19b2a50138
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-06-21 20:55:05 +1000

    added home.handlebars to temporarily render all games, added home routes and editProfile frontend JS

---
+++ public/js/editProfile.js
@@ -0,0 +1,34 @@
+const editProfile = async () => {
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const platform = document.getElementById("platform");
+  const discord_id = document.getElementById("discord_id").value;
+  const psn_id = document.getElementById("psn_id").value;
+  const xbox_id = document.getElementById("xbox_id").value;
+
+  const response = await fetch("/api/users/edit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      platform_id: platform,
+      discord_id: discord_id,
+      psn_id: psn_id,
+      xbox_id: xbox_id,
+    }),
+  });
+
+  if (response.ok) {
+    // bootstrap alert user edited
+    // redirect to profile page
+  } else {
+    // bootstrap alert for error
+    // redirect to profile page
+    alert(response.statusText)
+  }
+};
+
+document.querySelector("#edit-profile").addEventListener("click", editProfile);
