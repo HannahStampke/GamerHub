67e11684ae16d83d1c88f61c643bdf74cf5d0baf
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-06-24 15:18:34 +0930

    added home handlebars and game and posts partials

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,22 +4,40 @@ const { User, Game, Post, Comment, Genre } = require("../models");
 router.get('/', async (req, res) => {
     try {
         const gamesData = await Game.findAll({
-            include: [{
-                model: Post,
-            },
+            limit: 4,
+            include: [
             {
                 model: Genre,
+                attributes: ['genre_name']
             }
-        ]
+        ],
         });
 
+
+        const postData = await Post.findAll({
+            limit: 4,
+            order: [['session_time', 'DESC']],
+            include: [
+                {
+                    model: Game,
+                    attributes: ['game_name']
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+
         const games = gamesData.map((game) => game.get({plain: true}));
+        const posts = postData.map((post) => post.get({plain: true}));
 
+        // res.status(200).json(posts)
 
-        res.render('home', { games });
+        res.render('home', { games, posts, logged_in: req.session.logged_in });
 
     } catch (error) {
-        res.status(400).json(error)        
+        res.status(500).json(error)        
     }
 })
 
