fbeb74ca8b21976f43e281824512ab5a026dbc8f
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-06-25 11:07:37 +0930

    added view all games feature

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -45,6 +45,48 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/all-games', async (req, res) => {
+    try {
+        const gamesData = await Game.findAll({
+            order: Sequelize.literal('rand()') ,
+            include: [
+            {
+                model: Genre,
+                attributes: ['genre_name']
+            }
+        ],
+        });
+
+
+        const postData = await Post.findAll({
+            limit: 4,
+            order: [['session_time', 'DESC']],
+            include: [
+                {
+                    model: Game,
+                    attributes: ['game_name']
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+
+        const allGames = true
+        const games = gamesData.map((game) => game.get({plain: true}));
+        const posts = postData.map((post) => post.get({plain: true}));
+        const userName = req.session.username
+
+
+        res.render('home', { games, posts, logged_in: req.session.logged_in, userName, allGames});
+
+    } catch (error) {
+        res.status(500).json(error)        
+    }
+});
+
+
 router.get('/login', async (req, res) => {
     try {
         res.render('login', {logged_in: req.session.logged_in})
--- public/js/viewMoreGames.js
+++
@@ -1,8 +0,0 @@
-const viewMoreGames = async (event) => {
-    event.preventDefault();
-
-    console.log(event.target)
-
-} 
-
-document.querySelector("#more-games").addEventListener("click", viewMoreGames);
