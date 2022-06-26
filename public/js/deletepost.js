ce696e0b94d606b5b1de67a05a0fccee17e744cb
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-06-26 19:51:36 +0930

    delete post button working on profile page

--- public/js/deletepost.js
+++ public/js/deletepost.js
@@ -1,26 +1,28 @@
-
 // Delete post function
 const deletePost = async (event) => {
-    event.preventDefault();
-    // Get the post ID
-    const postId = event.target.id;
-    // Delete the identified post
-    try {
-        const response = await fetch(`/api/posts/${postId}`, {
-            method: "delete"
-        });
-        // error message
-        if (!response.ok) {
-            const message = 'Uh oh! ' + response.status;
-            throw new Error(message);
-        }
-        const data = await response.json();
-        console.log(data);
-    // catch error
-    } catch (error) {
-        console.log('Oh dear... ' + err);
-        }
+  event.preventDefault();
+  // Delete the identified post
+
+  const postId = event.target.getAttribute('data-postId')
+
+  try {
+    const response = await fetch(`/api/posts/${postId}`, {
+      method: "delete",
+    });
+    // error message
+    if (!response.ok) {
+      const message = "Uh oh! " + response.status;
+      throw new Error(message);
+    }else{
+        document.location.replace('/profile')
     }
+    const data = await response.json();
+    console.log(data);
+    // catch error
+  } catch (error) {
+    console.log("Oh dear... " + error);
+  }
+};
 
 // delete-post needs to be the the button name
 document.querySelector("#delete-post").addEventListener("click", deletePost);
